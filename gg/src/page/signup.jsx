8500ed6e5358dd87bded5c8e7b2f9935b767336c
gtra5 <147500556+gtra5@users.noreply.github.com>
2025-09-30 23:01:11 +0100

    Your commit message, e.g., Add new feature

--- gg/src/page/signup.jsx
+++ gg/src/page/signup.jsx
@@ -133,7 +133,7 @@ function Signup() {
       >
           {error && (
         <div className="w-full max-w-sm px-2 mb-2">
-          <div className="bg-red-100 fixed top-6 left-1/2 transform -translate-x-1/2  text-white px-6 py-3 rounded-lg shadow-lg z-50 transition-all" role="alert">
+          <div className="bg-red-500 fixed top-6 left-1/2 transform -translate-x-1/2  text-white px-6 py-3 rounded-lg shadow-lg z-50 transition-all" role="alert">
             {error}
           </div>
         </div>
